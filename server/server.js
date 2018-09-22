637e3671b49016be7a52ddd49d9de16da187245d
zow one <jeremie@10mentionweb.org>
2018-09-22 17:03:22 +0200

    heroku change

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
 
+var port = process.env.PORT || 3000
+
 const publicPath = path.join(__dirname , '../public');
 
 const app = express();
 
 app.use('/', express.static(publicPath)) //remplace le chemin de base de app
 
-app.listen(3000, ()=>{
-    console.log('listen  is up on', 3000);
+app.listen(port, ()=>{
+    console.log(`listen  is up on ${port}`);
 });
