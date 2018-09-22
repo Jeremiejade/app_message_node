10de6bf0dc903e6b7b8291e12630e836bcdc8a69
zow one <jeremie@10mentionweb.org>
2018-09-22 19:48:33 +0200

    osef

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+var socket = io();
+        socket.on('connect', ()=>{
+            console.log('conecter au server !');          
+           
+        });
+
+        socket.on('disconnect', ()=>{
+            console.log('disconnect');
+        });
+
+        socket.on('newMessage', (message)=>{
+            console.log(message);
+        });
+
+          
